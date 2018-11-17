90e6d3f87ebac5a6d9dfc7322cc9b506d84b634c
twinVectr <gokul2259@gmail.com>
2018-11-17 14:10:15 -0500

    format functions.php

--- functions.php
+++ functions.php
@@ -4,20 +4,10 @@
  * @package twinVectr-theme
  */
 
-use twinVectr\engine;
 define("ENGINE_DIR", "twinVectr-engine");
 
-
 // Require the Theme core file
-require_once(get_template_directory() . '/'. ENGINE_DIR .'/theme-start.php');
+require_once get_template_directory() . '/' . ENGINE_DIR . '/theme-start.php';
 
 // Call the Start method which instantiates the twin-vectr engine (theme)
 add_action('after_setup_theme', 'twinVectr\engine\start');
-
-
-
-
-
-
-
-
